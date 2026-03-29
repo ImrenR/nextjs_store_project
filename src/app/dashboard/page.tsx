65556f0f1c0f761e022ab325e15af49cb90b7e74
ImrenR <imrenrahbay@gmail.com>
2026-03-29 23:11:34 +0100

    update

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,8 +1,40 @@
-import React from 'react'
+import SummaryCard from '@/components/dashboard/SummaryCard';
+import { machines } from '@/lib/mock/data';
+import { Container, Grid, Typography } from '@mui/material';
+
 
 const DashboardPge = () => {
+   const totalMachines = machines.length;
+  const onlineMachines = machines.filter(
+    (machine) => machine.status === 'online'
+  ).length;
+
+  const lowStockProducts = machines.flatMap((machine) =>
+    machine.products.filter((product) => product.stock <= 3)
+  ).length;
+
+
   return (
-    <div> DashboardPge</div>
+    <Container sx={{py:4}}>
+      <Typography variant='h4' fontWeight="bold" mb={3}>
+        Smart Vending Dashboard
+      </Typography>
+      <Grid container spacing={2} mb={4}> 
+         <Grid size={{ xs: 12, md: 4 }}>
+          <SummaryCard title="Total Machines" value={totalMachines} />
+        </Grid>
+        <Grid size={{ xs: 12, md: 4 }}>
+          <SummaryCard title="Online Machines" value={onlineMachines}/>
+        </Grid>
+        <Grid size={{ xs: 12, md: 4 }}>
+          <SummaryCard title="Low Stock Products" value={lowStockProducts} />
+        </Grid>
+      </Grid>
+
+      <Typography>
+        Machines
+      </Typography>
+    </Container>
   )
 }
 
