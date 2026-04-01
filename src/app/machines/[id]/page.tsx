206a1274ce3d16e843dcee9e334369aaa2b6e6a7
ImrenR <imrenrahbay@gmail.com>
2026-04-01 01:13:18 +0100

    params promise

---
+++ src/app/machines/[id]/page.tsx
@@ -0,0 +1,36 @@
+import { machines } from "@/lib/mock/data";
+import { Container, Typography } from "@mui/material";
+
+type Props = {
+  params: Promise<{id:string}>}
+
+
+const MachineDetailsPage = async ({params}:Props) => {
+  
+  const {id} = await params; // paramstan id yi destruct ettik
+
+  // find the machines by id
+
+  const machine = machines.find((u)=> u.id === id);
+
+  if(!machine) {
+    return <div>Not found..</div>
+  }
+  
+  
+  return (
+    <Container sx={{py:4}}>
+      <Typography variant="h4" fontWeight="bold">
+{machine.name}
+      </Typography>
+      <Typography color="text.secondary">
+        {machine.location}
+      </Typography>
+      <Typography>
+        {machine.status}
+      </Typography>
+    </Container>
+  )
+}
+
+export default MachineDetailsPage
